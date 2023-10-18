1faf12383dac0adfe98707827a96a9be5457d423
lehuynhanhtai <lehuynhanhtai@gmail.com>
2023-10-18 15:38:51 +0700

    build UI

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}></div>
+      <div className={styles.logo}></div>
+      <div className={styles.links}></div>
+    </div>
+  );
+};
+
+export default Navbar;
