f492e16693684730eb874f6537b39ad7f285f078
lehuynhanhtai <lehuynhanhtai@gmail.com>
2023-10-19 21:25:31 +0700

    update context API and toggle

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import Footer from "@/components/footer/Footer";
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
 import Navbar from "@/components/navbar/Navbar";
+import { ThemeContextProvider } from "@/context/ThemeContext";
+import ThemeProvider from "@/providers/ThemeProvider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -14,13 +16,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <div className="container">
-          <div className="wrapper">
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
-        </div>
+        <ThemeContextProvider>
+          <ThemeProvider>
+            <div className="container">
+              <div className="wrapper">
+                <Navbar />
+                {children}
+                <Footer />
+              </div>
+            </div>
+          </ThemeProvider>
+        </ThemeContextProvider>
       </body>
     </html>
   );
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,3 +1,31 @@
-import { createContext } from "react";
+"use client";
+import { createContext, useEffect, useState } from "react";
 
-export default ThemeContext = createContext();
+export const ThemeContext = createContext();
+
+const getFromLocalStorage = () => {
+  if (typeof window !== "undefined") {
+    const value = localStorage.getItem("theme");
+    return value || "light";
+  }
+};
+
+export const ThemeContextProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => {
+    return getFromLocalStorage();
+  });
+
+  const toggle = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggle }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
