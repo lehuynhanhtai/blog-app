00a0588dce026ec83b718e410931c82e761c69f2
lehuynhanhtai <lehuynhanhtai@gmail.com>
2023-10-25 20:10:17 +0700

    UI post card

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import styles from "./card.module.css";
+import Link from "next/link";
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <Link href="/" className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+      </Link>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>11.2.2202 - </span>
+          <span className={styles.category}>CULTURE</span>
+        </div>
+
+        <Link href="/">
+          <h2>Tại sao nên sử dụng Next.js?</h2>
+        </Link>
+        <p className={styles.desc}>
+          Next.js là một open-source React front-end framework được bổ sung các
+          tính năng như Server Side Rendering (SSR) và Static Site Generation
+          (SSG). Next.js được xây dựng dựa trên thư viện React có nghĩa là
+          Next.js lấy những lợi thế của React và bổ sung các tính năng. Next.js
+          là một open-source React front-end framework được bổ sung các tính
+          năng như Server Side Rendering (SSR) và Static Site Generation (SSG).
+          Next.js được xây dựng dựa trên thư viện React có nghĩa là Next.js lấy
+          những lợi thế của React và bổ sung các tính năng.
+        </p>
+        <Link href="/" className={styles.link}>
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
