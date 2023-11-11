b979f464072ccbb557b77f33a33034cf1a9118b8
lehuynhanhtai <lehuynhanhtai@gmail.com>
2023-11-11 16:25:46 +0700

    update model, call api date categories

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,76 +1,36 @@
-"use client";
 import React from "react";
 import styles from "./categoryList.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import { findAllCategories } from "@/utils/callAPI";
+
+// Call API
+
+const data = await findAllCategories();
+
 const CategoryList = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
       <div className={styles.categories}>
-        <Link
-          href="/blog?cat=style"
-          className={`${styles.category} ${styles.style}`}
-        >
-          <Image
-            src="/style.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          style
-        </Link>
-        <Link href="/blog" className={`${styles.category} ${styles.fashion}`}>
-          <Image
-            src="/style.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          fashion
-        </Link>
-        <Link href="/blog" className={`${styles.category} ${styles.food}`}>
-          <Image
-            src="/style.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          food
-        </Link>
-        <Link href="/blog" className={`${styles.category} ${styles.travel}`}>
-          <Image
-            src="/style.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          travel
-        </Link>
-        <Link href="/blog" className={`${styles.category} ${styles.culture}`}>
-          <Image
-            src="/style.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          culture
-        </Link>
-        <Link href="/blog" className={`${styles.category} ${styles.coding}`}>
-          <Image
-            src="/style.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          coding
-        </Link>
+        {data?.map((item) => (
+          <Link
+            href="/blog?cat=style"
+            className={`${styles.category} ${styles[item.title]}`}
+            key={item._id}
+          >
+            {item.img && (
+              <Image
+                src={item.img}
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            )}
+            {item.title}
+          </Link>
+        ))}
       </div>
     </div>
   );
---
+++ src/utils/callAPI.js
@@ -0,0 +1,16 @@
+const url = "http://localhost:3000";
+
+export const findAllCategories = async () => {
+  const res = await fetch(url + "/api/categories", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json({
+    message: "get data successfully",
+    data: res,
+  });
+};
