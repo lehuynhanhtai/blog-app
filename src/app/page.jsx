65933bb07716eab9f9d1fcd9b92bbdece89ec2e6
lehuynhanhtai <lehuynhanhtai@gmail.com>
2023-11-13 20:19:43 +0700

    banner UI

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,10 +4,12 @@ import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
+import Banner from "@/components/banner/banner";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Banner />
       <Featured />
       <CategoryList />
       <div className={styles.content}>
---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,64 @@
+"use client";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import styles from "./banner.module.css";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
+// import required modules
+import { Navigation, Autoplay } from "swiper/modules";
+import Image from "next/image";
+
+const Banner = () => {
+  return (
+    <>
+      <Swiper
+        watchSlidesProgress
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        modules={[Navigation, Autoplay]}
+        className="mySwiper"
+        style={{ height: 600 }}
+      >
+        <SwiperSlide>
+          <Image
+            src="/book3.jpg"
+            alt=""
+            priority={true}
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
+            className={styles.image}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/book4.jpg"
+            alt=""
+            priority={true}
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
+            className={styles.image}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/book1.jpg"
+            alt=""
+            priority={true}
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
+            className={styles.image}
+          />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default Banner;
