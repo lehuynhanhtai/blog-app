c0d899cf2d02789bbbb7a4f62c3b39934a921aff
lehuynhanhtai <lehuynhanhtai@gmail.com>
2023-11-29 16:33:29 +0700

    API comment

---
+++ src/app/api/comment/route.js
@@ -0,0 +1,54 @@
+import prisma from "@/utils/connect";
+import { NextResponse } from "next/server";
+
+// GET COMMENTS
+export const GET = async (req) => {
+  const { searchParams } = new URL(req.url);
+  const postSlug = searchParams.get("postSlug");
+
+  try {
+    const comments = await prisma.comment.findMany({
+      where: {
+        ...(postSlug && { postSlug: postSlug }),
+      },
+      include: { user: true },
+    });
+
+    if (comments.length > 0) {
+      return new NextResponse(JSON.stringify(comments, { status: 200 }));
+    } else {
+      return new NextResponse(
+        JSON.stringify({ message: "No data found!" }, { status: 404 })
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
+
+export const POST = async (req) => {
+  const session = await getAuthSession();
+
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
+    );
+  }
+
+  try {
+    const body = await req.json();
+    const comment = await prisma.comment.create({
+      data: { ...body, userEmail: session.user.email },
+    });
+
+    return new NextResponse(JSON.stringify(comment, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
