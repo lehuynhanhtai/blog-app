c5b881f32a75343598b6c5fbf69dfcf5aa73e52e
lehuynhanhtai <lehuynhanhtai@gmail.com>
2023-12-29 15:43:31 +0700

    update code

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -9,7 +9,7 @@ const Featured = () => {
       <div className={styles.post}>
         <div className={styles.imgContainer}>
           <Image
-            src="/p1.jpeg"
+            src="/daidien.png"
             alt=""
             priority={true}
             fill
@@ -19,13 +19,13 @@ const Featured = () => {
         </div>
         <div className={styles.textContainer}>
           <h1 className={styles.postTitle}>
-            Lorem ipsum dolor sit amet alim consectetur adipisicing elit.
+            CHÀO MỪNG CÁC BẠN ĐÃ ĐẾN VỚI S-BLOG
           </h1>
           <p className={styles.postDesc}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Cupiditate, quam nisi magni ea laborum inventore voluptatum
-            laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium
-            quisquam! Harum unde sit culpa debitis.
+            S-BLOG là mạng xã hội, nơi chia sẽ quan điểm - kiến thức đến với
+            giới trẻ, S-BLOG đã phát triển một hệ sinh thái lớn mạnh, đa nền
+            tảng, với mục tiêu hỗ trợ và kết nối các cá nhân/nhóm tri thức trong
+            xã hội.
           </p>
           <button className={styles.button}>Read More</button>
         </div>
